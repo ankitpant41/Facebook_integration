93ee76a75fe3a5ddd877233b810d558dc68dbde3
ankit <74355737+ankitpant41@users.noreply.github.com>
2020-11-12 17:22:55 +0530

    add empty project

---
+++ Unity-Facebook/Assets/Fb_script.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Facebook.Unity;
+using System.ComponentModel;
+using UnityEngine.UI;
+
+public class Fb_script : MonoBehaviour
+{
+    public GameObject DialoggedIn;
+    public GameObject DialoggedOut;
+    public GameObject Dialogusername;
+    public GameObject Dialprofilepic;
+
+
+    void Awake()
+    {
+        FB.Init(SetInit, OnHideUnity);
+    }
+
+    void SetInit() {
+        if (FB.IsLoggedIn) {
+            Debug.Log("FB is looged in");
+        }
+
+        else
+        {
+            Debug.Log("FB is not looged in");
+
+        }
+        DealwithFBMenu(FB.IsLoggedIn);
+    }
+
+    void OnHideUnity(bool isGameShown) {
+
+        if (!isGameShown)
+        {
+            Time.timeScale = 0;
+        }
+        else {
+            Time.timeScale = 1;
+        }
+    }
+
+    public void FBlogin() {
+        List<string> permissions= new List<string> () ;
+        permissions.Add("public_profile"); 
+        FB.LogInWithPublishPermissions(permissions, AuthCallBack); 
+    }
+
+  void AuthCallBack(IResult result)
+    {
+        if (result.Error != null)
+        {
+            Debug.Log(result.Error);
+        }
+        else {
+            if (FB.IsLoggedIn)
+            {
+
+                Debug.Log("Fb is logged in");
+
+            }
+            else {
+                Debug.Log("Fb is not logged in");
+            }
+
+            DealwithFBMenu(FB.IsLoggedIn);
+        }
+    }
+
+    void DealwithFBMenu(bool isLoggedIn) {
+        if (isLoggedIn)
+        {
+            DialoggedIn.SetActive(true);
+            DialoggedOut.SetActive(false);
+            FB.API("/me?fields=first_name",HttpMethod.GET, DisplayUsername);
+            FB.API("/me/picture?type=square&height=128&width=128", HttpMethod.GET, DisplayProfilepic);
+        }
+        else {
+            DialoggedIn.SetActive(false);
+            DialoggedOut.SetActive(true);
+        }
+    
+    }
+
+    void DisplayUsername(IResult result) {
+        Text UserName = Dialogusername.GetComponent<Text> ();
+        if (result.Error == null) {
+            UserName.text = "Hi there, " + result.ResultDictionary ["first_name"]; 
+        }
+        else
+        {
+            Debug.Log(result.Error);
+        }
+
+    }
+    void DisplayProfilepic(IGraphResult result) {
+        if (result.Texture != null) {
+            Image Profilepic = Dialprofilepic.GetComponent<Image>();
+            Profilepic.sprite = Sprite.Create (result.Texture,new Rect(0,0,128,128), new Vector2());
+
+
+        }
+
+    }
+}
---
+++ Unity-Facebook/Assets/GameRequest.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Facebook.Unity;
+public class GameRequest : MonoBehaviour
+{
+    List<string> toID;
+    string fromID;
+    public void FacebookGameRequest()
+    {
+        /* List<string> facebookUserList1 = new List<string>();
+         //facebookUserList1.Add("702316293923967");//ls
+         facebookUserList1.Add("162942648597274");//personal
+         AppConfig.isChallengedGame = true;
+         FindObjectOfType<MainMenuScreen>().SelectedUsersToChallenge(facebookUserList1);
+         ColyseusClient.Instance.SendFrientRequest(facebookUserList1);
+         return;
+         */
+        FB.AppRequest(
+            "Come play Slap Fest!!",
+            null, null, null, null, null, null,
+            delegate (IAppRequestResult result)
+            {
+                fromID = result.RequestID;
+
+                List<string> str = new List<string>();
+                toID = new List<string>();
+                str.AddRange(result.To);
+                toID = str;
+
+                List<string> facebookUserList = new List<string>();
+                foreach (var item in toID)
+                {
+                    facebookUserList.Add(item);
+                }
+                //  AppConfig.isChallengedGame = true;
+                //  FindObjectOfType<MainMenuScreen>().SelectedUsersToChallenge(facebookUserList);
+                // ColyseusClient.Instance.SendFrientRequest(facebookUserList);
+            }
+        );
+    }
+}
